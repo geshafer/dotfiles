c5103aa05bf0772492e3b01a2e831fd32b8f24bc
Gale Shafer <gale@hey.com>
2021-12-12 16:56:05 -0500

    Add neovim configs

---
+++ config/nvim/init.lua
@@ -0,0 +1,147 @@
+local set = vim.opt
+
+vim.g.mapleader = ","
+
+set.backspace = "2"       -- Backspace deletes like most programs in insert mode
+set.history = 50
+set.ruler = true          -- show the cursor position all the time
+set.showcmd = true        -- display incomplete commands
+set.incsearch = true      -- do incremental searching
+set.laststatus = 2        -- Always display the status line
+set.autowrite = true      -- Automatically :write before running commands
+set.modelines = 0         -- Disable modelines as a security precaution
+set.guicursor = "i:block" -- Use block cursor in input mode
+
+require('bundles')
+
+-- Initialize Status Line
+require('lualine').setup({
+  options = {theme = 'dracula'},
+  sections = {
+    lualine_c = {
+      {'filename', path = 1}
+    }
+  }
+})
+
+-- Initialize TreeSitter
+require('nvim-treesitter.configs').setup({
+  ensure_installed = "maintained",
+  highlight = {enable = true},
+  indent = {enable = true},
+})
+
+-- Initialize Fuzzy Finder
+local telescope_actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = telescope_actions.close
+      },
+    },
+  }
+}
+require('telescope').load_extension('fzf')
+vim.api.nvim_set_keymap('n', '<Leader>t', '<cmd>Telescope find_files<cr>', {noremap = true})
+
+-- Initialize Git Gutter
+require('gitsigns').setup()
+
+-- Show hide file browser
+require('nvim-tree').setup()
+vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<cr>', {noremap = true, silent = true})
+vim.g.nvim_tree_show_icons = { folders = 1, folder_arrows = 1 }
+
+-- Comment with leader + /
+vim.api.nvim_set_keymap('n', '<Leader>/', ':Commentary<cr>', {noremap = true})
+vim.api.nvim_set_keymap('v', '<Leader>/', ':Commentary<cr>', {noremap = true})
+
+-- Remember last cursor position
+require('nvim-lastplace').setup({})
+
+-- matchit?
+-- Strip whitespace on save
+
+-- When the type of shell script is /bin/sh, assume a POSIX-compatible
+-- shell for syntax highlighting purposes.
+vim.g.is_posix = 1
+
+-- Softtabs, 2 spaces
+set.tabstop = 2
+set.shiftwidth = 2
+set.shiftround = true
+set.expandtab = true
+
+-- Display extra whitespace
+set.list = true
+set.listchars = {tab = '»·', trail = '·', nbsp = '·'}
+
+-- Make it obvious where 80 characters is
+set.textwidth = 80
+set.colorcolumn = "+1"
+
+-- Numbers
+set.number = true
+set.numberwidth = 5
+
+-- Tab completion
+set.wildmode = "list:longest,list:full"
+
+-- Treat <li> and <p> tags like the block tags they are
+vim.g.html_indent_tags = "li|p"
+
+-- Open new split panes to right and bottom, which feels more natural
+set.splitbelow = true
+set.splitright = true
+
+-- Quicker window movement
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true})
+
+-- Sane searching
+vim.api.nvim_set_keymap('n', '/', '/\\v', {noremap = true})
+vim.api.nvim_set_keymap('v', '/', '/\\v', {noremap = true})
+set.gdefault = true
+set.showmatch = true
+vim.api.nvim_set_keymap('n', '<tab>', '%', {noremap = true})
+vim.api.nvim_set_keymap('v', '<tab>', '%', {noremap = true})
+
+-- Clear search results
+vim.api.nvim_set_keymap('n', '<Leader><space>', ':noh<cr>', {noremap = true})
+
+-- Send all vim registers to the mac clipboard
+set.clipboard = "unnamed"
+
+-- Remap space to center on current line
+vim.api.nvim_set_keymap('n', '<space>', 'zz', {})
+
+-- Customize Airline
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_section_b = ''
+
+-- Highlight matches as you type.
+set.incsearch = true
+-- Highlight matches.
+set.hlsearch = true
+-- Case-insensitive searching.
+set.ignorecase = true
+-- But case-sensitive if expression contains a capital letter.
+set.smartcase = true
+
+-- Turn off line wrapping.
+set.wrap = false
+-- Show 3 lines of context around the cursor.
+set.scrolloff = 3
+
+-- Set the terminal's title
+set.title = true
+
+-- No beeping.
+set.visualbell = true
+
+-- Set persistent undo history
+set.undodir = vim.fn.stdpath('config') .. "/undo/"
+set.undofile = true
---
+++ config/nvim/lua/bundles.lua
@@ -0,0 +1,40 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug('hoob3rt/lualine.nvim')
+
+Plug('jlanzarotta/bufexplorer')
+
+Plug('numkil/ag.nvim')
+
+Plug('axelf4/vim-strip-trailing-whitespace')
+
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+
+-- This doesn't always work. If telescope is erroring do the following
+-- cd ~/.config/nvim/plugged/telescope-fzf-native.nvim && make
+Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'make'})
+
+-- Plug('nvim-lua/plenary.nvim')
+Plug('lewis6991/gitsigns.nvim')
+
+Plug('ethanholz/nvim-lastplace')
+
+Plug('kyazdani42/nvim-web-devicons')
+Plug('kyazdani42/nvim-tree.lua')
+
+Plug('pbrisbin/vim-mkdir')
+Plug('tpope/vim-commentary')
+Plug('tpope/vim-endwise')
+Plug('tpope/vim-eunuch')
+Plug('tpope/vim-fugitive')
+Plug('tpope/vim-projectionist')
+Plug('tpope/vim-rails')
+Plug('tpope/vim-rake')
+Plug('tpope/vim-repeat')
+
+vim.call('plug#end')
