d5d29645459a08e1173bca1efd7b87a6497d14ba
Gale Shafer <gale@hey.com>
2021-12-14 12:21:56 -0500

    Add default gitconfig per machine

--- install.sh
+++ install.sh
@@ -6,6 +6,14 @@ else
   ./machine/mac.sh
 fi
 
+if [ $SPIN ]; then
+  cp -f gitconfig-spin gitconfig-default
+elif hostname | grep -i Shopify &>/dev/null; then
+  cp -f gitconfig-shopify gitconfig-default
+else
+  cp -f gitconfig-personal gitconfig-default
+fi
+
 env RCRC=$HOME/dotfiles/rcrc rcup -f
 
 exec zsh
