b4a5f2ca74910c75806df5d3df5abe4c7db377ec
Gale Shafer <gale@hey.com>
2021-12-21 14:24:57 -0500

    Patch up custom git commands for Spin

--- zsh/functions/wut.zsh
+++ zsh/functions/wut.zsh
@@ -1,7 +1,7 @@
 wut() {
   nvim $(
     git diff --name-status $(git mom) |
-      awk '{ split($9, a, "**"); if ($1 != "D") { print $2; } }' &&
+      awk '{ split($9, a); if ($1 != "D") { print $2; } }' &&
       git ls-files --others --exclude-standard
   )
 }
