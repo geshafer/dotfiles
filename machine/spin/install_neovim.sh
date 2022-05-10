339989149115606d5c5b018f3bbeb3a7c34b178b
Gale Shafer <gale@hey.com>
2022-05-10 10:12:40 -0400

    Update Neovim in Spin

--- machine/spin/install_neovim.sh
+++ machine/spin/install_neovim.sh
@@ -9,13 +9,13 @@ sudo apt-get install -y \
   tmux
 
 # Fetch App Image for NeoVim
-NVIM_VERSION="v0.6.0"
+NVIM_VERSION="v0.7.0"
 mkdir -p $HOME/dotfiles/tmp
-cd $HOME/dotfiles/tmp
+cd /usr/local/src
 rm -rf nvim.appimage squashfs-root
-wget "https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim.appimage"
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
+sudo wget "https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim.appimage"
+sudo chmod u+x nvim.appimage
+sudo ./nvim.appimage --appimage-extract
 sudo rm -f /usr/local/bin/nvim
 sudo ln -s $PWD/squashfs-root/usr/bin/nvim /usr/local/bin/nvim
 
@@ -24,10 +24,16 @@ sudo gem install neovim
 npm -g install neovim
 
 if [[ ! -f /usr/local/bin/tree-sitter ]]; then
+  mkdir -p $HOME/dotfiles/tmp
+  cd $HOME/dotfiles/tmp
+
   # Install Tree-Sitter
-  TS_VERSION="v0.20.1"
+  TS_VERSION="v0.20.6"
   wget "https://github.com/tree-sitter/tree-sitter/releases/download/${TS_VERSION}/tree-sitter-linux-x64.gz"
   gunzip tree-sitter-linux-x64.gz
   chmod u+x tree-sitter-linux-x64
   sudo mv tree-sitter-linux-x64 /usr/local/bin/tree-sitter
 fi
+
+cd $HOME/dotfiles
+rm -rf $HOME/dotfiles/tmp
