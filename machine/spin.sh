0089c84df9db95819af5be1a259237a3812d226c
Gale Shafer <gale@hey.com>
2023-02-08 13:41:11 -0500

    Persist Spin Command History

--- machine/spin.sh
+++ machine/spin.sh
@@ -11,6 +11,11 @@ cartridge insert mcfly
 ln -sf ~/.data/cartridges/mcfly ~/.mcfly
 curl -fsSL https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sudo sh -s -- --git cantino/mcfly
 
+# add systemd timer for persisting mcfly history
+sudo cp machine/spin/persist-history.service /etc/systemd/system/persist-history.service
+sudo cp machine/spin/persist-history.timer /etc/systemd/system/persist-history.timer
+systemctl start persist-history.service
+
 # install neovim
 $HOME/dotfiles/machine/spin/install_neovim.sh
 
