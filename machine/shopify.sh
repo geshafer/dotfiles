391a6a9c63d1bd028e0ee730cfcb64d4a154e6fc
Gale Shafer <gale@hey.com>
2024-03-18 14:57:01 -0400

    Install homebrew by default

--- machine/shopify.sh
+++ machine/shopify.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Install expected apps
 brew bundle --file=- <<EOF
 brew "ffmpeg"
