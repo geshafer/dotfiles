d65022df8a07eac3be3b611f1ff3328df158220a
Gale Shafer <gale@hey.com>
2024-07-02 09:34:57 -0400

    Configure lualine

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,10 +23,25 @@ vim.cmd([[colorscheme monokai-pro]])
 require('lualine').setup({
   options = {theme = 'monokai-pro'},
   sections = {
+    lualine_a = {'mode'},
+    lualine_b = {},
     lualine_c = {
       {'filename', path = 1}
-    }
-  }
+    },
+    lualine_x = {},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      {'filename', path = 1}
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
 })
 
 -- Initialize TreeSitter
