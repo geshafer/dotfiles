c9eb5c28b884939b46b2ae9a3cc0551865d04c1b
Gale Shafer <gale@hey.com>
2024-11-30 14:18:44 -0500

    Improve buffer explorer

--- config/nvim/lua/plugins/buffer_explorer.lua
+++ config/nvim/lua/plugins/buffer_explorer.lua
@@ -8,12 +8,30 @@ return {
       { '<leader>be', ':JABSOpen<cr>', desc = 'Open buffer explorer' },
     },
     opts = {
+      height = 16,
+      width = 100,
       relative = 'editor',
       position = {'center', 'center'},
+      sort_mru = true, -- most recently used
+
       keymap = {
+        close = 'd',
         preview = 'i',
       },
-      sort_mru = true,
+
+      symbols = {
+        current = " ◦",
+        split = " ◦",
+        alternate = " •",
+        hidden = " •",
+
+        locked = "",
+        ro = "",
+        edited = "",
+        terminal = "",
+        default_file = "",
+        terminal_symbol = "",
+      },
     },
   },
 }
