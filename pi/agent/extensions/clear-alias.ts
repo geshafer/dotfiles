642354396a21b0a11127756ec029ef52825b305c
Gale Shafer <gale@hey.com>
2026-03-05 08:20:30 -0500

    Fixup Pi

---
+++ pi/agent/extensions/clear-alias.ts
@@ -0,0 +1,10 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default function (pi: ExtensionAPI) {
+  pi.registerCommand("clear", {
+    description: "Start a new session (alias for /new)",
+    handler: async (_args, ctx) => {
+      await ctx.newSession();
+    },
+  });
+}
